5376dce1d6dd3b557736fa74085b85896ca5e12e
mgatland <mgatland@gmail>
2014-01-25 16:28:55 +1300

    refactor: move creature to separate file

--- js/game.js
+++ js/game.js
@@ -1,36 +1,8 @@
 "use strict";
-require(["pagelink"], function(util) {
+require(["creature"], function(Creature) {
 
 	var Action = function () {
 
-	}
-
-	var Creature = function (i, name, cover, energy, aim, dodge, leadership) {
-		this.i = i;
-		this.name = name;
-		this.cover = cover;
-		this.maxCover = cover;
-		this.energy = energy;
-		this.maxEnergy = energy;
-		this.aim = aim;
-		this.dodge = dodge;
-		this.leadership = leadership;
-
-		this.actions = [];
-
-
-		this.draw = function () {
-			getElement("name").innerHTML = this.name;
-			getElement("cover").innerHTML = "Cover: " + this.cover + "/" + this.maxCover;
-			getElement("energy").innerHTML = "Energy: " + this.energy + "/" + this.maxEnergy;
-			getElement("aim").innerHTML = "Aim: " + this.aim;
-			getElement("dodge").innerHTML = "Dodge: " + this.dodge;
-			getElement("leadership").innerHTML = "Leadership: " + this.leadership;
-		}
-
-		var getElement = function(ele) {
-			return document.querySelector(".p" + i + " > ." + ele);
-		}
 	}
 
 	var creatures = [];
