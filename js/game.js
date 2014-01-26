ace1359017fa1b634e60e2146c852bc1885cb81b
mgatland <mgatland@gmail>
2014-01-26 14:43:57 +1300

    added character portraits

--- js/game.js
+++ js/game.js
@@ -6,8 +6,8 @@ $( document ).ready( function () {
 
 	var keyboard = new Keyboard();
 	var creatures = [];
-	creatures[0] = new Creature(0, "Matthew", 10, 5, 5, 5, 5, creatures, false);
-	creatures[1] = new Creature(1, "Amanda", 10, 4, 5, 4, 6, creatures, false);
+	creatures[0] = new Creature(0, "Riley", 10, 5, 5, 5, 5, creatures, false);
+	creatures[1] = new Creature(1, "Brooklyn", 10, 4, 5, 4, 6, creatures, false);
 	creatures[2] = new Creature(2, "Bandit", 5, 5, 5, 5, 5, creatures, true);
 	creatures[3] = new Creature(3, "Bandit Leader", 5, 5, 5, 5, 5, creatures, true);
 
